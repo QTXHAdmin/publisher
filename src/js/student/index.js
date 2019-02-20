d03df41f7924ff6a8589ac9268e9e3b2383e841e
YIQIAN <1215414707@qq.com>
2019-02-20 22:34:25 +0000

    added the module page

--- src/js/student/index.js
+++ src/js/student/index.js
@@ -15,17 +15,17 @@ require.config({
 require(['jquery', 'Swiper'], function($, Swiper) {
     $(function() {
         initBannerArea();
-
-        function initBannerArea() {
-            var mySwiper = new Swiper('.swiper-container', {
-                direction: 'horizontal', // 垂直切换选项
-                loop: true, // 循环模式选项
-                autoplay: true,
-                // 如果需要分页器
-                pagination: {
-                    el: '.swiper-pagination'
-                }
-            });
-        }
     });
+
+    function initBannerArea() {
+      var mySwiper = new Swiper('.swiper-container', {
+          direction: 'horizontal', // 水平切换选项
+          loop: true, // 循环模式选项
+          autoplay: true,
+          // 如果需要分页器
+          pagination: {
+              el: '.swiper-pagination'
+          }
+      });
+  }
 });
